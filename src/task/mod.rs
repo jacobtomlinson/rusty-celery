84c9f93bbdbd327203144bb1561a4a5870043979
Jacob Tomlinson <jtomlinson@nvidia.com>
2025-04-24 13:02:39 +0000

    Fix up some DateTime deprecation warnings

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -114,15 +114,14 @@ pub trait Task: Send + Sync + std::marker::Sized {
                 let now_secs = now.as_secs() as u32;
                 let now_millis = now.subsec_millis();
                 let eta_secs = now_secs + countdown;
-                Some(DateTime::<Utc>::from_naive_utc_and_offset(
-                    NaiveDateTime::from_timestamp_opt(eta_secs as i64, now_millis * 1000)
+                Some(
+                    DateTime::<Utc>::from_timestamp(eta_secs as i64, now_millis * 1000)
                         .ok_or_else(|| {
                             TaskError::UnexpectedError(format!(
                                 "Invalid countdown seconds {countdown}",
                             ))
                         })?,
-                    Utc,
-                ))
+                )
             }
             Err(_) => None,
         };
@@ -153,8 +152,7 @@ pub trait Task: Send + Sync + std::marker::Sized {
                 let now_millis = now.subsec_millis();
                 let eta_secs = now_secs + delay_secs;
                 let eta_millis = now_millis + delay_millis;
-                NaiveDateTime::from_timestamp_opt(eta_secs as i64, eta_millis * 1000)
-                    .map(|eta| DateTime::<Utc>::from_naive_utc_and_offset(eta, Utc))
+                DateTime::<Utc>::from_timestamp(eta_secs as i64, eta_millis * 1000)
             }
             Err(_) => None,
         }
